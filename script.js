ad8916513832f83b6cf88ff3c5a500d8b230c7b3
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-10 21:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let a = prompt("Enter first number");
+let b = prompt("Enter second number");
+
+	if(a!=undefined && b!=undefined)
+         document.getElementById('output').innerText = parseInt(a)+parseInt(b);
+	else
+		document.getElementById('output').innerText = "Invalid input. Please enter a valid number.";
